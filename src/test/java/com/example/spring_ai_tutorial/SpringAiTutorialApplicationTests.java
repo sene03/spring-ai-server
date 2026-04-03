91083e4b144d27c81ccc26f14442b1259145d1ce
sene03 <hansel0311@gmail.com>
2026-04-03 16:17:21 +0900

    fix: add properties for test

--- src/test/java/com/example/spring_ai_tutorial/SpringAiTutorialApplicationTests.java
+++ src/test/java/com/example/spring_ai_tutorial/SpringAiTutorialApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.spring_ai_tutorial;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SpringAiTutorialApplicationTests {
+
+    @MockBean
+    VectorStore vectorStore;
 
     @Test
     void contextLoads() {
